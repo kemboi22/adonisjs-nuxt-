0322c9632d59c5f4e034b6d6eb704307658da024
kemboi22 <kemboielvis22@gmail.com>
2024-04-01 02:05:25 +0300

    made Login Interactive

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import type {User} from "~/types";
 
+const authStore = useAuthStore()
+const formData = ref<User>(<User>{})
 </script>
 
 <template>
@@ -13,29 +16,33 @@
       </CardDescription>
     </CardHeader>
     <CardContent>
-      <div class="grid gap-4">
-        <div class="grid gap-2">
-          <Label for="email">Email</Label>
-          <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-          />
-        </div>
-        <div class="grid gap-2">
-          <div class="flex items-center">
-            <Label for="password">Password</Label>
-            <a href="#" class="ml-auto inline-block text-sm underline">
-              Forgot your password?
-            </a>
+      <form @submit.prevent="authStore.login(formData)">
+        <div class="grid gap-4">
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                v-model="formData.email"
+                required
+            />
           </div>
-          <Input id="password" type="password" required />
+          <div class="grid gap-2">
+            <div class="flex items-center">
+              <Label for="password">Password</Label>
+              <a href="#" class="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </a>
+            </div>
+            <Input id="password" type="password" required v-model="formData.password"/>
+          </div>
+          <Button type="submit" class="w-full">
+            {{ authStore.loading ? 'Loading ....' : 'Login' }}
+          </Button>
         </div>
-        <Button type="submit" class="w-full">
-          Login
-        </Button>
-      </div>
+      </form>
+
       <div class="mt-4 text-center text-sm">
         Don't have an account?
         <NuxtLink to="/auth/register" class="underline">
